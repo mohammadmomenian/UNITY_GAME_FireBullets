1ea4297de2266060c66f07d7b4316ce21b8a6b3a
Mohammad Momenian <bossofcode@gmail.com>
2019-11-27 22:49:52 +0200

    Initial commit

---
+++ Assets/Scripts/MobileUICtrl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileUICtrl : MonoBehaviour {
+
+    public GameObject player;
+
+    PlayerCtrl playerCtrl;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerCtrl = player.GetComponent<PlayerCtrl>();
+    }
+
+    public void MobileMoveLeft()
+    {
+        playerCtrl.MobileMoveLeft();
+    }
+
+    public void MobileMoveRight()
+    {
+        playerCtrl.MobileMoveRight();
+    }
+
+    public void MobileMoveStop()
+    {
+        playerCtrl.MobileStop();
+    }
+
+    public void MobileFireBullets()
+    {
+        playerCtrl.MobileFireBullets();
+    }
+
+    public void MobileJump()
+    {
+        playerCtrl.MobileJump();
+    }
+}
